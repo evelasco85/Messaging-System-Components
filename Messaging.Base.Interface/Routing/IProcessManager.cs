367bbd2b218dcff668f5066b44f99478525ec442
Dev_2 <Dev_2>
2016-10-07 15:49:47 +0800

    Refactor process manager

--- Messaging.Base.Interface/Routing/IProcessManager.cs
+++ Messaging.Base.Interface/Routing/IProcessManager.cs
@@ -6,6 +6,21 @@ using System.Threading.Tasks;
 
 namespace Messaging.Base.Routing
 {
+    public delegate void NotifyManagerDelegate<TKey, TData, TProcessor>(IProcess<TKey, TData, TProcessor> process);
+
+    public interface IProcess<TKey, TData, TProcessor>
+    {
+        TProcessor Processor { get; set; }
+        TKey GetKey();
+        TData GetProcessData();
+        void UpdateManager();
+        IProcessManager<TKey, TData, TProcessor> ProcessManager { get; set; }
+        NotifyManagerDelegate<TKey, TData, TProcessor> ManagerNotifier { get; set; }
+        TProcessor GetProcessor();
+        void StartProcess();
+        void StartProcess(TProcessor processor);
+    }
+
     public interface IProcessManager<TKey, TData, TProcessor>
     {
         void AddProcess(IProcess<TKey, TData, TProcessor> process);
