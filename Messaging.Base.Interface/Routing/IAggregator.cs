369af8a71542635767985130931d99ac8a9142bf
Dev_2 <Dev_2>
2016-10-07 16:59:44 +0800

    Refactor Aggregator

--- Messaging.Base.Interface/Routing/IAggregator.cs
+++ Messaging.Base.Interface/Routing/IAggregator.cs
@@ -6,6 +6,20 @@ using System.Threading.Tasks;
 
 namespace Messaging.Base.Routing
 {
+    public delegate void OnNotifyAggregationCompletion<TAggregatedValue>(TAggregatedValue aggregatedValue);
+
+    public interface IAggregate<TKey, TValue, TAggregatedValue>
+    {
+        TKey Key { get; set; }
+        void AggregateValue(TValue value);
+        bool IsComplete();
+        void PreAggregation(ref TValue value);
+        void PostAggregation(TValue value);
+        TAggregatedValue PerformAggregation(IList<TValue> aggregatedValues);
+        TAggregatedValue GetAggregatedValue();
+        void NotifyAggregationCompletion();
+    }
+
     public interface IAggregator<TKey, TValue, TAggregatedValue, TAggregate>
         where TAggregate : IAggregate<TKey, TValue, TAggregatedValue>
     {
