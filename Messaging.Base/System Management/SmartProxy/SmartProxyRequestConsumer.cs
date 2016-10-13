c21290107ce138b4ced72b5fefa552a74e2c3be2
Dev_2 <Dev_2>
2016-10-13 15:42:32 +0800

    Added SmartProxy files

---
+++ Messaging.Base/System Management/SmartProxy/SmartProxyRequestConsumer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging.Base.System_Management.SmartProxy
+{
+    public interface ISmartProxyRequestConsumer
+    {
+        
+    }
+
+    public class SmartProxyRequestConsumer : ISmartProxyRequestConsumer
+    {
+    }
+}
