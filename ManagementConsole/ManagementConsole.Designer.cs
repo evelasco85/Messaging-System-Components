bade90458acae1cc8b048451917ff82bf3c255e2
Enrico <enrico_velasco@hotmail.com>
2016-11-01 19:37:21 +0800

    Amend UI placement

--- ManagementConsole/ManagementConsole.Designer.cs
+++ ManagementConsole/ManagementConsole.Designer.cs
@@ -28,98 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtLoanBrokerResponseData = new System.Windows.Forms.TextBox();
-            this.txtLoanBrokerStatData = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label3 = new System.Windows.Forms.Label();
             this.txtCreditBureauStatus = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLoanBrokerStatData = new System.Windows.Forms.TextBox();
+            this.txtLoanBrokerResponseData = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // txtLoanBrokerResponseData
+            // groupBox1
             // 
-            this.txtLoanBrokerResponseData.Location = new System.Drawing.Point(12, 299);
-            this.txtLoanBrokerResponseData.Multiline = true;
-            this.txtLoanBrokerResponseData.Name = "txtLoanBrokerResponseData";
-            this.txtLoanBrokerResponseData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtLoanBrokerResponseData.Size = new System.Drawing.Size(473, 146);
-            this.txtLoanBrokerResponseData.TabIndex = 0;
-            // 
-            // txtLoanBrokerStatData
-            // 
-            this.txtLoanBrokerStatData.Location = new System.Drawing.Point(12, 500);
-            this.txtLoanBrokerStatData.Multiline = true;
-            this.txtLoanBrokerStatData.Name = "txtLoanBrokerStatData";
-            this.txtLoanBrokerStatData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtLoanBrokerStatData.Size = new System.Drawing.Size(473, 168);
-            this.txtLoanBrokerStatData.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 283);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(171, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Load Broker Response Data:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 484);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(161, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Load Broker Statistic Data:";
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtCreditBureauStatus);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtLoanBrokerStatData);
+            this.groupBox1.Controls.Add(this.txtLoanBrokerResponseData);
+            this.groupBox1.Location = new System.Drawing.Point(445, 33);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(621, 617);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Running Data";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 99);
+            this.label3.Location = new System.Drawing.Point(88, 30);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(134, 13);
-            this.label3.TabIndex = 5;
+            this.label3.TabIndex = 11;
             this.label3.Text = "Monitor Credit Bureau:";
             // 
             // txtCreditBureauStatus
             // 
-            this.txtCreditBureauStatus.Location = new System.Drawing.Point(12, 115);
+            this.txtCreditBureauStatus.Location = new System.Drawing.Point(88, 46);
             this.txtCreditBureauStatus.Multiline = true;
             this.txtCreditBureauStatus.Name = "txtCreditBureauStatus";
             this.txtCreditBureauStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.txtCreditBureauStatus.Size = new System.Drawing.Size(473, 146);
-            this.txtCreditBureauStatus.TabIndex = 4;
+            this.txtCreditBureauStatus.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(88, 415);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Load Broker Statistic Data:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(88, 214);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Load Broker Response Data:";
+            // 
+            // txtLoanBrokerStatData
+            // 
+            this.txtLoanBrokerStatData.Location = new System.Drawing.Point(88, 431);
+            this.txtLoanBrokerStatData.Multiline = true;
+            this.txtLoanBrokerStatData.Name = "txtLoanBrokerStatData";
+            this.txtLoanBrokerStatData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLoanBrokerStatData.Size = new System.Drawing.Size(473, 168);
+            this.txtLoanBrokerStatData.TabIndex = 7;
+            // 
+            // txtLoanBrokerResponseData
+            // 
+            this.txtLoanBrokerResponseData.Location = new System.Drawing.Point(88, 230);
+            this.txtLoanBrokerResponseData.Multiline = true;
+            this.txtLoanBrokerResponseData.Name = "txtLoanBrokerResponseData";
+            this.txtLoanBrokerResponseData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLoanBrokerResponseData.Size = new System.Drawing.Size(473, 146);
+            this.txtLoanBrokerResponseData.TabIndex = 6;
             // 
             // ManagementConsole
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(497, 701);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtCreditBureauStatus);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtLoanBrokerStatData);
-            this.Controls.Add(this.txtLoanBrokerResponseData);
+            this.ClientSize = new System.Drawing.Size(1089, 701);
+            this.Controls.Add(this.groupBox1);
             this.Name = "ManagementConsole";
             this.Text = "Management Console";
             this.Load += new System.EventHandler(this.ManagementConsole_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtLoanBrokerResponseData;
-        private System.Windows.Forms.TextBox txtLoanBrokerStatData;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtCreditBureauStatus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLoanBrokerStatData;
+        private System.Windows.Forms.TextBox txtLoanBrokerResponseData;
     }
 }
 
--- ManagementConsole/ManagementConsole.cs
+++ ManagementConsole/ManagementConsole.cs
@@ -53,83 +53,70 @@ namespace ManagementConsole
 
         void ProcessMessage(Message message)
         {
-            using (StreamReader reader = new StreamReader(message.BodyStream))
+            if (InvokeRequired)
             {
-                XmlDocument messageDoc = new XmlDocument();
-
-                messageDoc.LoadXml(reader.ReadToEnd());
-
-                XmlNode root = messageDoc.SelectSingleNode("/*");
-
-                switch (root.Name)
+                this.Invoke(new MethodInvoker(() =>
                 {
-                    case "SummaryStat":
-                        PrependStatisticData(messageDoc);
-                        break;
-                    case "LoanBrokerProxyInfo":
-                        PrependResponseData(messageDoc);
-                        break;
-                    case "MonitorStatus":
-                        PrependMonitorData(messageDoc);
-                        break;
-                    default:
-                        break;
-                }
+                    using (StreamReader reader = new StreamReader(message.BodyStream))
+                    {
+                        XmlDocument messageDoc = new XmlDocument();
+
+                        messageDoc.LoadXml(reader.ReadToEnd());
+
+                        XmlNode root = messageDoc.SelectSingleNode("/*");
+
+                        switch (root.Name)
+                        {
+                            case "SummaryStat":
+                                PrependStatisticData(messageDoc);
+                                break;
+                            case "LoanBrokerProxyInfo":
+                                PrependResponseData(messageDoc);
+                                break;
+                            case "MonitorStatus":
+                                PrependMonitorData(messageDoc);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }));
             }
         }
 
         void PrependResponseData(XmlDocument messageDoc)
         {
-            if (InvokeRequired)
-            {
-                this.Invoke(new MethodInvoker(() =>
-                {
-                    XmlNode data = messageDoc.SelectSingleNode("/LoanBrokerProxyInfo/Detail");
+                XmlNode data = messageDoc.SelectSingleNode("/LoanBrokerProxyInfo/Detail");
 
-                    this.txtLoanBrokerResponseData.Text = data.InnerText + Environment.NewLine + this.txtLoanBrokerResponseData.Text;
-                }));
-            }
+                this.txtLoanBrokerResponseData.Text = data.InnerText + Environment.NewLine + this.txtLoanBrokerResponseData.Text;
         }
 
         void PrependStatisticData(XmlDocument messageDoc)
         {
+            XmlNode duration = messageDoc.SelectSingleNode("/SummaryStat/AverageReplyDuration");
+            XmlNode request = messageDoc.SelectSingleNode("/SummaryStat/AverageOutstandingRequest");
 
-            if (InvokeRequired)
-            {
-                this.Invoke(new MethodInvoker(() =>
-                {
-                    XmlNode duration = messageDoc.SelectSingleNode("/SummaryStat/AverageReplyDuration");
-                    XmlNode request = messageDoc.SelectSingleNode("/SummaryStat/AverageOutstandingRequest");
+            string data = string.Format("Ave. Reply Duration [{0:0.###}] | Ave. Outstanding Request [{1:0.###}]",
+                Convert.ToDouble(duration.InnerText),
+                Convert.ToDouble(request.InnerText)
+                );
 
-                    string data = string.Format("Ave. Reply Duration [{0:0.###}] | Ave. Outstanding Request [{1:0.###}]",
-                        Convert.ToDouble(duration.InnerText),
-                        Convert.ToDouble(request.InnerText)
-                        );
-
-                    this.txtLoanBrokerStatData.Text = data + Environment.NewLine + this.txtLoanBrokerStatData.Text;
-                }));
-            }
+            this.txtLoanBrokerStatData.Text = data + Environment.NewLine + this.txtLoanBrokerStatData.Text;
         }
 
         void PrependMonitorData(XmlDocument messageDoc)
         {
-            if (InvokeRequired)
-            {
-                this.Invoke(new MethodInvoker(() =>
-                {
-                    XmlNode statusNode = messageDoc.SelectSingleNode("/MonitorStatus/Status");
-                    XmlNode descriptionNode = messageDoc.SelectSingleNode("/MonitorStatus/Description");
-                    string message = string.Format("Status: {2}{0}Description: {3}{0}Date-Time: {4}{0}{1}",
-                        Environment.NewLine,
-                        string.Join("", Enumerable.Repeat('-', 10)),
-                        statusNode.InnerText,
-                        descriptionNode.InnerText,
-                        DateTime.Now.ToString("T")
-                        );
+            XmlNode statusNode = messageDoc.SelectSingleNode("/MonitorStatus/Status");
+            XmlNode descriptionNode = messageDoc.SelectSingleNode("/MonitorStatus/Description");
+            string message = string.Format("Status: {2}{0}Description: {3}{0}Date-Time: {4}{0}{1}",
+                Environment.NewLine,
+                string.Join("", Enumerable.Repeat('-', 10)),
+                statusNode.InnerText,
+                descriptionNode.InnerText,
+                DateTime.Now.ToString("T")
+                );
 
-                    this.txtCreditBureauStatus.Text = message + Environment.NewLine + this.txtCreditBureauStatus.Text;
-                }));
-            }
+            this.txtCreditBureauStatus.Text = message + Environment.NewLine + this.txtCreditBureauStatus.Text;
         }
 
         private void ManagementConsole_Load(object sender, EventArgs e)
