6b8bee73cf61c03afb0603df3bb08f970d2c8975
Dev_2 <Dev_2>
2016-11-14 15:49:25 +0800

    Added sample signal R implementation(non-msmq implementation

---
+++ Web/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web.Startup))]
+namespace Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            //Install-Package Microsoft.AspNet.SignalR
+            //Install-Package Microsoft.AspNet.SignalR.Sample
+            //Browse to ~/SignalR.Sample/StockTicker.html in two browsers and click the Open Market button.
+            Microsoft.AspNet.SignalR.StockTicker.Startup.ConfigureSignalR(app);
+        }
+    }
+}
