370567f5504019682f741bf6ffe6f2e7cad2f7ac
Enrico <enrico_velasco@hotmail.com>
2016-12-08 22:40:35 +0800

    Added interface

---
+++ Messaging.Base.Interface/Transformation/ICanonicalDataModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Messaging.Base.Transformation
+{
+    public interface ICanonicalDataModel<TMessage, TEntity>
+    {
+        TMessage TranslateToMessage(TEntity entity);
+        TEntity TranslateToEntity(TMessage message);
+        TMessage GetMessage(TEntity entity);
+        TEntity GetEntity(TMessage message);
+    }
+}
